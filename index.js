d001559d706a495f4ed2c2b653f3c1000e45d1fa
Mykola Kashubyak <kashubyak514@gmail.com>
2025-12-01 13:30:30 +0200

    Add /users endpoint to return a list of users

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000
 
 app.use(express.json())
 
 app.get('/ping', (req, res) => {
 	res.send('pong')
+})
+
+app.get('/users', (req, res) => {
+	const users = [
+		{ id: 1, name: 'Alice', role: 'JEDI' },
+		{ id: 2, name: 'Bob', role: 'SITH' },
+	]
+	res.json(users)
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
 })
