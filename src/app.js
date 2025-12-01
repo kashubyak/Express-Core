e3bda85d35d2bf52827d19cf5687b5c99bf3db06
Mykola Kashubyak <kashubyak514@gmail.com>
2025-12-01 15:29:21 +0200

    Add Joi validation and error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,24 +1,4 @@
-const express = require('express')
-const usersRouter = require('./modules/users/users.routes')
-const app = express()
+const errorHandler = require('./middlewares/errorHandler')
 
-app.use(express.json())
-
-app.use((req, res, next) => {
-	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
-	next()
-})
-
-app.get('/ping', (req, res) => res.send('pong'))
-app.use('/users', usersRouter)
-
-app.use((req, res) => {
-	res.status(404).json({ message: 'Route not found' })
-})
-
-app.use((err, req, res, next) => {
-	console.error(err.stack)
-	res.status(500).json({ message: 'Internal Server Error' })
-})
-
+app.use(errorHandler)
 module.exports = app
