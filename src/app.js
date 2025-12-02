26f02441efc170653de7eb99bd281431a00aa5a5
Mykola Kashubyak <kashubyak514@gmail.com>
2025-12-02 14:48:49 +0200

    feat: enhance app setup by organizing middleware usage and improving request logging

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const requestId = require('./middlewares/requestId')
+const logger = require('./utils/logger')
+const notFoundHandler = require('./middlewares/notFoundHandler')
+const errorHandler = require('./middlewares/errorHandler')
 const usersRouter = require('./modules/users/users.routes')
 const authRouter = require('./modules/auth/auth.routes')
-const errorHandler = require('./middlewares/errorHandler')
-const notFoundHandler = require('./middlewares/notFoundHandler')
 
 const app = express()
 
+app.use(helmet())
+app.use(cors())
 app.use(express.json())
+app.use(requestId)
 
 app.use((req, res, next) => {
-	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
+	logger.info(`Incoming Request`, {
+		method: req.method,
+		url: req.url,
+		requestId: req.id,
+	})
 	next()
+})
+
+app.get('/health', (req, res) => {
+	res.status(200).json({ status: 'OK', uptime: process.uptime() })
 })
 
 app.get('/ping', (req, res) => res.send('pong'))
